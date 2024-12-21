d65096dba6bc1ad09c6898aab35f1cf400588c97
Kanav4002 <kumarkanav5753@gmail.com>
2024-12-21 22:02:59 +0530

    Form creation, input fields

---
+++ frontend/script.js
@@ -0,0 +1,24 @@
+const togglePassowrdVisibility = (inputELement, toggleElement) => {
+  if(inputELement.type === 'password') {
+    inputELement.type = 'text';
+    toggleElement.innerHTML = '<i class="fa fa-eye-slash"></i>';
+  } else {
+    inputELement.type = 'password';
+    toggleElement.innerHTML = '<i class="fa fa-eye"></i>';
+  }
+}
+
+const passwordInput = document.getElementById('password');
+const togglePassword = document.getElementById('togglePassword');
+const passwordConfirm = document.getElementById('passwordConfirm');
+const togglePasswordConfirm = document.getElementById('togglePasswordConfirm');
+
+togglePassword.addEventListener('click', () => {
+  togglePassowrdVisibility(passwordInput, togglePassword);
+});
+
+togglePasswordConfirm.addEventListener('click', () => {
+  togglePassowrdVisibility(passwordConfirm, togglePasswordConfirm);
+});
+
+
